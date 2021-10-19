476a32a4bd7d90e27eadc6fd49fff5b48410227a
aa02015t <aa02015t@dell7060test>
2021-10-19 17:32:51 +0200

    ddddd

--- structure.h
+++ structure.h
@@ -3,13 +3,21 @@
 #define T 4 
 
 
-typedef int piece[T][T]; 
+typedef struct {
+
+    short int forme[T][T];
+    short int posx;
+    short int posy;
+
+}piece; 
 typedef enum{ A, B, C, D, E, F}FORME;
 
 void init_box(piece box, int t);
 void affiche_box(piece box, int t);
 
 void dessine_forme(piece box, FORME l);
+
+void tourne_piece(piece box, FORME l);
 
 
 
