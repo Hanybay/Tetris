3864016a3fdcea38baa86e5f27540fb8cd14ffcd
aa02015t <aa02015t@dell7060test>
2021-10-19 17:13:38 +0200

    mmmmmmmmmmmmmmmmmmmmmmmmm

--- structure.c
+++ structure.c
@@ -3,6 +3,26 @@
 #include <MLV/MLV_all.h>
 #include "structure.h"
 
+void multiplication(piece result, int m[][], piece box, int tm, int tb){
+
+    int i, j
+
+}
+void tourne_piece(piece box, FORME l){
+
+    int m[2][2]; /* matrice multiplication */
+    piece result[4][4];
+    init_box(result, 4);
+    m[0][0]=0;
+    m[0][1]=-1;
+    m[1][0]=1;
+    m[1][1]=0;
+    
+    
+
+    multiplication(result, m, box, 2, 4); 
+
+}
 
 void init_box(piece box, int t){
 
@@ -28,24 +48,19 @@ void affiche_box(piece box, int t){
 
 void dessine_forme(piece box, FORME l){
     switch (l){
-        case A:
-            box[3][3]=1;box[3][2]=1;box[3][1]=1;box[3][0]=1;
-            break;
-        case B:
-            box[2][1]=1;box[3][1]=1;box[3][2]=1;box[3][3]=1;
-            break;
-        case C:
-            box[2][3]=1;box[3][1]=1;box[3][2]=1;box[3][3]=1;
-            break;
-        case D:
-            box[2][1]=1;box[3][1]=1;box[3][2]=1;box[2][2]=1;
-            break;
-        case E:
-            box[2][1]=1;box[2][2]=1;box[3][2]=1;box[3][3]=1;
-            break;
-        case F:
-            box[3][1]=1;box[2][3]=1;box[2][2]=1;box[3][2]=1;
-            break;
+        case A: box[3][3]=1;box[3][2]=1;box[3][1]=1;box[3][0]=1;break;
+        case B: box[2][1]=1;box[3][1]=1;box[3][2]=1;box[3][3]=1;break;
+        case C: box[2][3]=1;box[3][1]=1;box[3][2]=1;box[3][3]=1;break;
+        case D: box[2][1]=1;box[3][1]=1;box[3][2]=1;box[2][2]=1;break;
+        case E: box[2][1]=1;box[2][2]=1;box[3][2]=1;box[3][3]=1;break;
+        case F: box[3][1]=1;box[2][3]=1;box[2][2]=1;box[3][2]=1;break;
     }
 }
 
+void form_alea(piece box){
+
+
+}
+
+
+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
